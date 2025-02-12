c8af3a8d1a89afce4ca7f4d706409d389f6046b2
Will Brennan <wbrennan899@gmail.com>
2025-02-12 11:01:17 -0800

    create VideoFileComponent from FileComponent

--- src/backend/base/langflow/components/data/__init__.py
+++ src/backend/base/langflow/components/data/__init__.py
@@ -12,6 +12,7 @@ __all__ = [
     "CSVToDataComponent",
     "DirectoryComponent",
     "FileComponent",
+    "VideoFileComponent",
     "JSONToDataComponent",
     "SQLExecutorComponent",
     "URLComponent",
